a648c2ef566c3290f300f3b5a143ed2f53891fd6
oscarsweatman2 <oscarsweatman@gmail.com>
2017-02-09 13:52:14 -0600

    Adding Change Level Script
    Simple script to load a specified level when a key is pressed

---
+++ Minecraft/Assets/Scripts/ChangeLevelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevelScript : MonoBehaviour {
+
+    public string levelName = "";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Application.LoadLevel(levelName);
+        }
+	}
+}
