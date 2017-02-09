bbda7829a3c81c695a94bbc3ed2103f0c689a369
CoolWeek2017-8 <user8.coolweek@spamgourmet.com>
2017-02-09 13:07:32 -0600

    minion update

--- Minecraft/Assets/Scripts/towerScript.cs
+++ Minecraft/Assets/Scripts/towerScript.cs
@@ -4,103 +4,51 @@ using UnityEngine;
 
 public class towerScript : MonoBehaviour
 {
-    public int m_allegiance;
-    public bool m_isMine;
-    public bool m_isNeutral;
-    public bool m_isTheirs;
-    public bool m_completecontroll;
-    public bool m_completeTheirs;
+    public Minion.Allegiance m_allegiance;
     public int m_minionSpawnNumber;
     public int m_minionSpawnRate = 5;
     // Use this for initialization
     public Minion m_spawnMinion;
     public float spawnTimer = 2;
     public int spawnCount = 1;
-
+    public int m_towerArmor = 5;
     void Start()
     {
-        if (m_allegiance >= -2 && m_allegiance <= 2) {
-            m_isNeutral = true;
-        }
-        else {
-            m_isNeutral = false;
-
-        }
-        if (m_allegiance < -2) {
-            m_isTheirs = true;
-        }
-        else {
-            m_isTheirs = false;
-
-        }
-        if (m_allegiance > 2) {
-            m_isMine = true;
-        }
-        else {
-            m_isMine = false;
-
-        }
-        if (m_allegiance == 5) {
-            m_completecontroll = true;
-        }
-        else {
-            m_completecontroll = false;
-
-        }
-        if (m_allegiance == -5) {
-            m_completeTheirs = true;
-        }
-        else {
-            m_completeTheirs = false;
-        }
+        
         spawnTimer = m_minionSpawnRate;
     }
     // Update is called once per frame
     void Update()
     {
-        if (m_allegiance >= -2 && m_allegiance <= 2) {
-            m_isNeutral = true;
-        }
-        else {
-            m_isNeutral = false;
-
-        }
-        if (m_allegiance < -2) {
-            m_isTheirs = true;
-        }
-        else {
-            m_isTheirs = false;
-
-        }
-        if (m_allegiance > 2) {
-            m_isMine = true;
-        }
-        else {
-            m_isMine = false;
-
-        }
-        if (m_allegiance == 5) {
-            m_completecontroll = true;
-        }
-        else {
-            m_completecontroll = false;
-
-        }
-        if (m_allegiance == -5) {
-            m_completeTheirs = true;
-        }
-        else {
-            m_completeTheirs = false;
-        }
+        
         spawnTimer -= Time.deltaTime;
         if (spawnTimer <= 0)
         {
-            
+
             //Debug.Log("Wave " + spawnCount + " Spawned!");
-            spawnTimer = m_minionSpawnRate;
+            Minion[] minionlist = GameObject.FindObjectsOfType<Minion>();
+            List<Minion> enemyMinionList = new List<Minion>();
+            List<Minion> playerMinionList = new List<Minion>();
+
+
+            foreach (Minion minion in minionlist)
+            {
+                if(minion.miniononplayerteam)
+                {
+                    playerMinionList.Add(minion);
+                }
+                else
+                {
+                    enemyMinionList.Add(minion);
+                }
+            }
+
+
+
+                spawnTimer = m_minionSpawnRate;
             for (int i = 0; i < 5; i++) {
                 int minionNum = GameObject.FindObjectsOfType<Minion>().Length;
-                if (minionNum < 10) {
+                if (minionNum < 10 && m_spawnMinion != null) {
                     GameObject.Instantiate(m_spawnMinion, this.transform.position + new Vector3(Random.Range(1, 10), Random.Range(1, 5), 0), Quaternion.identity);
                 }
             }
