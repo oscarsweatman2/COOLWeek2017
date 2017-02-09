9f2d3b682cda4da405df925f6b8e0e5aac164862
CoolWeek2017-7 <user7.coolweek@spamgourmet.com>
2017-02-09 14:37:46 -0600

    No longer capped to 10 minions total on the field

--- Minecraft/Assets/Scripts/towerScript.cs
+++ Minecraft/Assets/Scripts/towerScript.cs
@@ -6,37 +6,40 @@ public class towerScript : MonoBehaviour
 {
     // Use this for initialization
     public Minion.Allegiance m_teamAllegiance;
-    public int m_minionSpawnNumber;
+    public int m_minionSpawnNumber = 3;
     public int m_minionSpawnRate = 5;
-    public Minion m_spawnMinion;
+    //public Minion m_spawnMinion;
+    public Minion m_blueMinion;
+    public Minion m_redMinion;
     public float spawnTimer = 2;
     public int spawnCount = 1;
     public int m_towerArmor = 5;
     public float levelUpTimer = 2;
     public int m_levelUpCount = 1;
+    
 
     void Start()
     {
         spawnTimer = m_minionSpawnRate;
         levelUpTimer = m_levelUpCount;
     }
-	
+
     // Update is called once per frame
     void Update()
     {
         spawnTimer -= Time.deltaTime;
-        
+
         if (spawnTimer <= 0)
         {
             spawnTimer = m_minionSpawnRate;
-			
+
             Minion[] minionlist = GameObject.FindObjectsOfType<Minion>();
             List<Minion> enemyMinionList = new List<Minion>();
             List<Minion> playerMinionList = new List<Minion>();
 
             foreach (Minion minion in minionlist)
             {
-                if(minion.miniononplayerteam)
+                if (minion.miniononplayerteam)
                 {
                     playerMinionList.Add(minion);
                 }
@@ -45,34 +48,39 @@ public class towerScript : MonoBehaviour
                     enemyMinionList.Add(minion);
                 }
             }
-
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < m_minionSpawnNumber; i++)
             {
-                int minionNum = GameObject.FindObjectsOfType<Minion>().Length;
-                if (minionNum < 10)
+                //int minionNum = GameObject.FindObjectsOfType<Minion>().Length;
+                if (m_teamAllegiance == Minion.Allegiance.BLUE)
                 {
-                    if (m_spawnMinion != null)
+                    if (m_blueMinion != null)
                     {
-                        Minion spawnedMinion = GameObject.Instantiate(m_spawnMinion, this.transform.position + new Vector3(Random.Range(1, 10), Random.Range(1, 5), 0), Quaternion.identity);
+                        Minion spawnedBlueMinion = GameObject.Instantiate(m_blueMinion, this.transform.position + new Vector3(Random.Range(1, 10), Random.Range(1, 5), 0), Quaternion.identity);
+                    }
+                }
+                else if (m_teamAllegiance == Minion.Allegiance.RED)
+                {
+                    if (m_redMinion != null)
+                    {
+                        Minion spawnedRedMinion = GameObject.Instantiate(m_redMinion, this.transform.position + new Vector3(Random.Range(1, 10), Random.Range(1, 5), 0), Quaternion.identity);
                     }
                 }
             }
         }
-		
+
         levelUpTimer -= Time.deltaTime;
-        if (levelUpTimer <= 0) {
+        if (levelUpTimer <= 0)
+        {
             levelUpTimer = m_levelUpCount;
             levelUp();
         }
     }
-
     void levelUp()
     {
         if (m_teamAllegiance != Minion.Allegiance.NEUTRAL)
         {
             m_towerArmor += 5;
         }
-
         int curLevel = getCurrentLevel();
         if (curLevel == 1)
         {
@@ -98,5 +106,46 @@ public class towerScript : MonoBehaviour
     public int getCurrentLevel()
     {
         return m_towerArmor / 5 + 1;
+    }
+    void towerHasBeenAttacked(Minion m)
+    {
+        //if (m_teamAllegiance == Minion.Allegiance.NEUTRAL)
+        //{
+        //    //switch to the side of the minion that got destroyed
+        //    //if (m.miniononplayerteam == true) 
+        //    //{
+        //    //   this.m_towerArmor += 1;
+        //    //}
+        //    //else if (m.miniononplayerteam == false)
+        //    //{
+        //    //   this.m_towerArmor -= 1;
+        //    //}
+        
+        //}
+        //if (m_teamAllegiance == Minion.Allegiance.BLUE)
+        //{
+        //    if (m.miniononplayerteam == true)
+        //    {
+        //        //friendly minion, do nothing
+        //    }
+        //    else
+        //    {
+        //        //tower lose X armor
+        //        this.m_towerArmor -= 1;
+        //    }
+        //}
+        //if (m_teamAllegiance == Minion.Allegiance.RED)
+        //{
+        //    if (m.miniononplayerteam == true)
+        //    {
+        //        //friendly minion, do nothing
+        //    }
+        //    else
+        //    {
+        //        //tower lose X armor
+        //        this.m_towerArmor += 1;
+        //    }
+        //}
+     
     }
 }
